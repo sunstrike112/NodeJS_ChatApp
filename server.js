ca6282cf8ca75540ce7652aa83cd972e413a8fa2
phaptran <phaptran112@gmail.com>
2021-10-16 21:12:12 +0700

    setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+const hostname = "localhost"
+const port = 8017
+
+app.get("/hellobugcreator", (req, res) => {
+  res.send("<h1>Hello Bug Creator</h1>")
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Hello Bug Creator, I am running at ${hostname}: ${port}/`)
+})
