e905a58f77b7d7efb1a4635e614abbf8859bb186
phaptran <phaptran112@gmail.com>
2021-10-20 02:02:31 +0700

    add routes and controllers

---
+++ src/controllers/authController.js
@@ -0,0 +1,10 @@
+let getLoginRegister = (req, res) => {
+  return res.render("auth/loginRegister")
+}
+
+let getLogout = (req, res) => {}
+
+module.exports = {
+  getLoginRegister: getLoginRegister,
+  getLogout: getLogout,
+}
