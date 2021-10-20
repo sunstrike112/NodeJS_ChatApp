5e263fd0464d421278179c1e6afbf31e37cc9518
phaptran <phaptran112@gmail.com>
2021-10-21 01:49:19 +0700

    register user part 1

---
+++ lang/vi.js
@@ -0,0 +1,7 @@
+export const transValidation = {
+  email_incorrect: "Email phải có dạng abc@gmail.com",
+  gender_incorrect: "Giới tính bị sai",
+  password_incorrect:
+    "Mật khẩu chứa ít nhất 8 ký tự (gồm chữ hoa, thường, số và ký tự đặc biệt)",
+  password_confirmation_incorrect: "Mật khẩu không khớp",
+}
