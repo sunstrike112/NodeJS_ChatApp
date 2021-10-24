2280c6d2efd053bfcdfc875da9f57423392b73d9
phaptran <phaptran112@gmail.com>
2021-10-24 23:47:38 +0700

    send confirm email account

--- lang/vi.js
+++ lang/vi.js
@@ -10,10 +10,25 @@ export const transErrors = {
   account_in_use: "Email đã được sử dụng",
   account_removed: "Tài khoản này đã gỡ khỏi hệ thống, chi tiết xin liên hệ bộ phận CSKH",
   account_not_active: "Tài khoản chưa được kích hoạt, vui lòng kiểm tra email của bạn để kích hoạt tài khoản",
+  token_undefined: "Token không tồn tại"
 }
 
 export const transSuccess = {
   userCreated: (userEmail) => {
     return `Tài khoản <strong>${userEmail}</strong> đã được tạo, vui lòng kiểm tra email để kích hoạt tài khoản`
-  }
+  },
+  account_actived: "Kích hoạt tài khoản thành công, bạn có thể đăng nhập vào ứng dụng"
+}
+
+export const transMail = {
+  subject: "Awesome Chat by Bug Creator : Xác nhận kích hoạt tài khoản",
+  template: (linkVerify) => {
+    return `
+      <h2>Bạn nhận được email này vì đã đăng ký tài khoản trên ứng dựng Awesome Chat by Bug Creator</h2>
+      <h3>Vui lòng click vào liên kết để kích hoạt tài khoản</h3>
+      <h3><a href="${linkVerify}" targer="blank">${linkVerify}</a></h3>
+      <h4>Nếu đây là sự nhầm lẫn hãy bỏ qua thông báo này</h4>
+    `
+  },
+  send_failed: "Có lỗi trong quá trình gửi email, vui lòng liên hệ bộ phận CSKH"
 }
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,8 +22,26 @@ let postRegister = async (req, res) => {
   }
 
   try {
-    let createUserSuccess = await auth.register(req.body.email, req.body.gender, req.body.password)
+    let createUserSuccess = await auth.register(
+      req.body.email, req.body.gender,
+      req.body.password, req.protocol,
+      req.get("host"))
     successArr.push(createUserSuccess)
+    req.flash("success", successArr)
+    return res.redirect("/login-register")
+  } catch (error) {
+    errorArr.push(error)
+    req.flash("errors", errorArr)
+    return res.redirect("/login-register")
+  }
+}
+
+let verifyAccount = async (req, res) => {
+  let errorArr = []
+  let successArr = []
+  try {
+    let verifySuccess = await auth.verifyAccount(req.params.token)
+    successArr.push(verifySuccess)
     req.flash("success", successArr)
     return res.redirect("/login-register")
   } catch (error) {
@@ -36,4 +54,5 @@ let postRegister = async (req, res) => {
 module.exports = {
   getLoginRegister: getLoginRegister,
   postRegister: postRegister,
+  verifyAccount: verifyAccount
 }
