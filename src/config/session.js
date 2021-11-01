6f41112e900aec8e74151df9155e988d5a8fb41d
BugCreator <phaptran112@gmail.com>
2021-11-01 20:16:18 +0700

    setup Socket.io library

--- src/config/session.js
+++ src/config/session.js
@@ -1,7 +1,7 @@
-import session from "express-session"
-import connectMongo from "connect-mongo"
+import session from "express-session";
+import connectMongo from "connect-mongo";
 
-let MongoStore = connectMongo(session)
+let MongoStore = connectMongo(session);
 
 /**
  * This variable is where save session, in this case is mongoDB
@@ -10,24 +10,29 @@ let sessionStore = new MongoStore({
   url: `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   autoReconnect: true,
   // autoRemove: "native"
-})
+});
 
 /**
  * Config session for app
  * @param app from exactly express module
  */
 
-let configSession = (app) => {
-  app.use(session({
-    key: "express.sid",
-    secret: "mySecret",
-    store: sessionStore,
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 // 1 day
-    }
-  }))
-}
+let config = (app) => {
+  app.use(
+    session({
+      key: process.env.SESSION_KEY,
+      secret: process.env.SESSION_SECRET,
+      store: sessionStore,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
+      },
+    })
+  );
+};
 
-module.exports = configSession
+module.exports = {
+  config: config,
+  sessionStore: sessionStore,
+};
